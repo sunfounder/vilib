6da9693180148107af17f1130d0e3ff9b1bdd936
dino <845864704@qq.com>
2021-10-20 10:58:08 +0800

    add displa switches of local and web

--- examples/display.py
+++ examples/display.py
@@ -1,8 +1,9 @@
+from threading import local
 from vilib import Vilib
 
 def main():
-    Vilib.camera_start()
-    Vilib.display()
+    Vilib.camera_start(inverted_flag=False)
+    Vilib.display(local=True,web=True)
 
 if __name__ == "__main__":
     main()
