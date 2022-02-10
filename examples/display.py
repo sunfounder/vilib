4a224c59202917908f02080b8003dee421d0def0
dino <845864704@qq.com>
2022-02-10 16:29:20 +0800

    add tip

--- examples/display.py
+++ examples/display.py
@@ -4,6 +4,7 @@ from vilib import Vilib
 def main():
     Vilib.camera_start(vflip=False,hflip=False)
     Vilib.display(local=True,web=True)
-
+    print('\npress Ctrl+C to exit')
+    
 if __name__ == "__main__":
     main()
