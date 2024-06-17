021d94eb0ee995611f52d3528e3ef633ffedf143
Lovmong <1967471283@qq.com>
2024-06-17 15:02:23 +0800

    add load_labels

---
+++ vilib/utils.py
@@ -0,0 +1,47 @@
+import os
+
+# utils
+# =================================================================
+def run_command(cmd):
+    import subprocess
+    p = subprocess.Popen(
+        cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    result = p.stdout.read().decode('utf-8')
+    status = p.poll()
+    return status, result
+
+def getIP():
+    wlan0 = os.popen("ifconfig wlan0 |awk '/inet/'|awk 'NR==1 {print $2}'").readline().strip('\n')
+    eth0 = os.popen("ifconfig eth0 |awk '/inet/'|awk 'NR==1 {print $2}'").readline().strip('\n')
+
+    if wlan0 == '':
+        wlan0 = None
+    if eth0 == '':
+        eth0 = None
+
+    return wlan0,eth0
+
+def check_machine_type():
+    import platform
+    machine_type = platform.machine()
+    if machine_type == "armv7l":
+        return 32, machine_type
+    elif machine_type == "aarch64":
+        return 64, machine_type
+    else:
+        raise ValueError(f"[{machine_type}] not supported")
+
+def load_labels(path):
+    """Loads the labels file. Supports files with or without index numbers."""
+    import re
+
+    with open(path, 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+        labels = {}
+        for row_number, content in enumerate(lines):
+            pair = re.split(r'[:\s]+', content.strip(), maxsplit=1)
+            if len(pair) == 2 and pair[0].strip().isdigit():
+                labels[int(pair[0])] = pair[1].strip()
+            else:
+                labels[row_number] = pair[0].strip()
+    return labels
