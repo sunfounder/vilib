31dbe3ae2ba289254b93914431ec54c6ca3a4311
Lovmong <1967471283@qq.com>
2024-06-17 15:05:34 +0800

    optimize hands_detection

--- examples/hands_detection.py
+++ examples/hands_detection.py
@@ -3,14 +3,24 @@ from time import sleep
 from vilib import Vilib
 
 def main():
-    Vilib.camera_start(vflip=False,hflip=False)
-    Vilib.display(local=True,web=True)
+    Vilib.camera_start(vflip=False, hflip=False)
+    Vilib.show_fps()
+    Vilib.display(local=True, web=True)
     Vilib.hands_detect_switch(True)
+    sleep(1)
 
-    # while True:
-    #     print(Vilib.detect_obj_parameter['hands_joints']) # Print finger joint coordinates
-    #     sleep(0.5)
+    while True: 
+        # print(Vilib.detect_obj_parameter['hands_joints']) # Print finger joint coordinates
+        sleep(0.5)
+
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
+    except Exception as e:
+        print(f"\033[31mERROR: {e}\033[m")
+    finally:
+        Vilib.camera_close()
 
