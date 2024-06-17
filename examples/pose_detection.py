10428ec821e37860c87390826e935fb6487d8ce5
Lovmong <1967471283@qq.com>
2024-06-17 15:06:00 +0800

    optimize pose_detection

--- examples/pose_detection.py
+++ examples/pose_detection.py
@@ -1,14 +1,27 @@
 #!/usr/bin/env python3
 from vilib import Vilib
-
+from time import sleep
 
 def main():
     
     Vilib.camera_start(vflip=False,hflip=False)
+    Vilib.show_fps()
     Vilib.display(local=True,web=True)
     Vilib.pose_detect_switch(True)
+    sleep(1)
+
+    while True: # Keep the main program running
+        sleep(0.5)
+
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
+    except Exception as e:
+        print(f"\033[31mERROR: {e}\033[m")
+    finally:
+        Vilib.camera_close()
 
 
