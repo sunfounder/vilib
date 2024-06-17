e196f3015bebeee91c6219171164b25d483c3c65
Lovmong <1967471283@qq.com>
2024-06-17 15:06:23 +0800

    add show_fps

--- examples/display.py
+++ examples/display.py
@@ -1,19 +1,38 @@
 #!/usr/bin/env python3
 from vilib import Vilib
+import time
 
 def main():
-    Vilib.camera_start(vflip=False,hflip=False) # vflip:vertical flip, hflip:horizontal Flip
-    # local:local display, web:web display
-    # when local=True, the image window will be displayed on the system desktop
-    # when web=True, the image window will be displayed on the web browser at http://localhost:9000/mjpg
-    Vilib.display(local=True,web=True) 
-    print('\npress Ctrl+C to exit')
+    Vilib.camera_start(vflip=False, hflip=False) # vflip:vertical flip, hflip:horizontal Flip
+
+    Vilib.show_fps()
+    '''
+    Vilib.show_fps(color=(0, 255, 0), fps_size=0.68, fps_origin=(10, 20))
     
+    color: fps font color, (r, g, b)
+    fps_size, font size
+    fps_origin, origin position, (width, height)
+
+    Vilib.hide_fps() # Hide the FPS
+    '''
+
+    Vilib.display(local=True, web=True) 
+    '''
+    local:local display, web:web display
+    when local=True, the image window will be displayed on the system desktop
+    when web=True, the image window will be displayed on the web browser at http://localhost:9000/mjpg
+    '''
+
+    print('\npress Ctrl+C to exit')
+
+
+    while True:
+        # do something
+        time.sleep(1)
+
 if __name__ == "__main__":
     try:
         main()
-        while True:
-            pass
     except KeyboardInterrupt:
         pass
     except Exception as e:
