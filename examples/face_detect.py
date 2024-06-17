4f4ffdba24e6eafafee6e05cc2395789424d33ad
Lovmong <1967471283@qq.com>
2024-06-17 14:46:41 +0800

    optimize face detection

--- examples/face_detect.py
+++ examples/face_detect.py
@@ -2,18 +2,45 @@
 from vilib import Vilib
 from time import sleep
 
+'''
+Vilib.face_detect_switch(True)     # True / False
+
+Vilib.face_obj_parameter['x']      # the largest face block center coordinate 
+Vilib.face_obj_parameter['y']      # the largest face block center coordinate 
+Vilib.face_obj_parameter['w']      # the largest face block pixel width 
+Vilib.face_obj_parameter['h']      # the largest face block pixel height 
+Vilib.face_obj_parameter['n']      # Number of face blocks found 
+  
+'''
+
 def main():
-    Vilib.camera_start(vflip=False,hflip=False)
-    Vilib.display(local=True,web=True)
+    Vilib.camera_start(vflip=False, hflip=False)
+    Vilib.show_fps()
+    Vilib.display(local=True, web=True)
     Vilib.face_detect_switch(True)
     sleep(1)
 
     while True:
-        n = Vilib.detect_obj_parameter['human_n'] 
-        print("%s faces are found."%n)   
-        sleep(1)
+        n = Vilib.face_obj_parameter['n']
+        if n != 0:
+            x = Vilib.face_obj_parameter['x']
+            y = Vilib.face_obj_parameter['y']
+            w = Vilib.face_obj_parameter['w']
+            h = Vilib.face_obj_parameter['h']
+            print(f"{n} faces found, the largest block coordinate=({x}, {y}), size={w}*{h}")
+        else:
+            print(f'No face found')
+        sleep(0.5)
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
+    except Exception as e:
+        print(f"\033[31mERROR: {e}\033[m")
+    finally:
+        Vilib.camera_close()
+
 
     
---
+++ vilib/face_detection.py
@@ -0,0 +1,125 @@
+import cv2
+# https://github.com/opencv/opencv-python
+
+'''Define parameters for face detection object'''
+# Default model path
+face_model_path = '/opt/vilib/haarcascade_frontalface_default.xml'
+# face_model_path = '/opt/vilib/haarcascade_profileface.xml'
+
+face_obj_parameter = {}
+face_obj_parameter['x'] = 320  # the largest face block center x-axis coordinate
+face_obj_parameter['y'] = 240  # the largest face block center y-axis coordinate
+face_obj_parameter['w'] = 0  # the largest face block pixel width
+face_obj_parameter['h'] = 0  # the largest face pixel height
+face_obj_parameter['n'] = 0  # Number of faces detected
+
+
+def set_face_detection_model(model_path):
+    '''
+    Set face detection model path
+
+    :param model_path: The path of face haar-cascade XML classifier file
+    :type model_path: str
+    '''
+    global face_model_path
+    face_model_path = model_path
+
+def face_detect(img, width, height, rectangle_color=(255, 0, 0)):
+    '''
+    Face detection with opencv
+
+    :param img: The detected image data
+    :type img: list
+    :param width: The width of the image data
+    :type width: int
+    :param height: The height of the image data
+    :type height: int
+    :param rectangle_color: The color (BGR, tuple) of rectangle. Eg: (255, 0, 0).
+    :type color_name: tuple
+    :returns: The image returned after detection.
+    :rtype: Binary list
+    '''
+    # Reduce image for faster recognition 
+    zoom = 2
+    width_zoom = int(width / zoom)
+    height_zoom = int(height / zoom)
+    resize_img = cv2.resize(img, (width_zoom, height_zoom), interpolation=cv2.INTER_LINEAR)
+    
+    # Converting the image to grayscale
+    gray_img = cv2.cvtColor(resize_img, cv2.COLOR_BGR2GRAY) 
+
+    # Loading the haar-cascade XML classifier file
+    face_cascade = cv2.CascadeClassifier(face_model_path)
+
+    # Applying the face detection method on the grayscale image
+    faces = face_cascade.detectMultiScale(gray_img, scaleFactor=1.3, minNeighbors=3)
+    
+    face_obj_parameter['n'] = len(faces)
+
+    # Iterating over all detected faces
+    if face_obj_parameter['n'] > 0:
+        max_area = 0
+        for (x,y,w,h) in faces:
+            x = x * zoom
+            y = y * zoom
+            w = w * zoom
+            h = h * zoom
+            # Draw rectangle around the face
+            cv2.rectangle(img, (x, y), (x+w, y+h), rectangle_color, 2)
+            
+            # Save the attribute of the largest color block
+            object_area = w * h
+            if object_area > max_area: 
+                max_area = object_area
+                face_obj_parameter['x'] = int(x + w/2)
+                face_obj_parameter['y'] = int(y + h/2)
+                face_obj_parameter['w'] = w
+                face_obj_parameter['h'] = h
+    else:
+        face_obj_parameter['x'] = width/2
+        face_obj_parameter['y'] = height/2
+        face_obj_parameter['w'] = 0
+        face_obj_parameter['h'] = 0
+        face_obj_parameter['n'] = 0
+        
+    return img
+
+# Test
+def test():
+    print("face detection ...")
+
+    cap = cv2.VideoCapture(0)
+    cap.set(3, 640)
+    cap.set(4, 480)
+
+    while cap.isOpened():
+        success,frame = cap.read()
+        if not success:
+            print("Ignoring empty camera frame.")
+            # If loading a video, use 'break' instead of 'continue'.
+            continue
+
+        # frame = cv2.flip(frame, -1) # Flip camera vertically
+
+        out_img = face_detect(frame, 640, 480)
+
+        cv2.imshow('Face detecting ...', out_img)
+
+        # if cv2.waitKey(1) & 0xFF == ord('q'):
+        #     break
+        # if cv2.waitKey(1) & 0xff == 27: # press 'ESC' to quit
+        #     break
+        # if cv2.getWindowProperty('Face detecting ...', 1) < 0:
+        #     break
+
+        key = cv2.waitKey(10) & 0xff
+        print(key)
+
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    test()
+
+
